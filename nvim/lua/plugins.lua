715bd2654a1d90df71f9cac72adb9a964e3c7f58
Wendell Sun <iwendellsun@gmail.com>
2022-10-31 15:54:17 +0900

    Refactor nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -14,26 +14,10 @@ return require('packer').startup(function()
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
   use {'neovim/nvim-lspconfig', 'williamboman/nvim-lsp-installer'}
-
-  -- nvim-cmp
-  use 'hrsh7th/cmp-nvim-lsp' -- { name = nvim_lsp }
-  use 'hrsh7th/cmp-buffer'   -- { name = 'buffer' },
-  use 'hrsh7th/cmp-path'     -- { name = 'path' }
-  use 'hrsh7th/cmp-cmdline'  -- { name = 'cmdline' }
-  use 'hrsh7th/nvim-cmp'
-  -- vsnip
-  use 'hrsh7th/cmp-vsnip'    -- { name = 'vsnip' }
-  use 'hrsh7th/vim-vsnip'
-  use 'rafamadriz/friendly-snippets'
-  -- lspkind
-  use 'onsails/lspkind-nvim'
-
   -- autopairs
   use 'windwp/nvim-autopairs'
   -- neoscroll
   use 'karb94/neoscroll.nvim'
-  -- lsp_signature
-  use "ray-x/lsp_signature.nvim"
   -- telescope
   use {
     'nvim-telescope/telescope.nvim',
@@ -46,4 +30,5 @@ return require('packer').startup(function()
         require('Comment').setup()
     end
   }
+  use {'neoclide/coc.nvim', branch = 'release'}
 end)
--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,6 +1,6 @@
 require'nvim-tree'.setup {
     -- 关闭文件时自动关闭
-    auto_close = false,
+    -- auto_close = false,
     -- 不显示 git 状态图标
     git = {
         enable = false
