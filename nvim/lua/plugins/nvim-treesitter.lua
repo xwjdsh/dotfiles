c5180000d487e14cd49efda76fd50f55a090d21f
Wen Sun <iwendellsun@gmail.com>
2024-02-07 15:11:36 +0900

    Add vimdoc treesitter

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,17 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "html", "css", "vim", "lua", "javascript", "typescript", "go", "rust", "yaml", "json" },
+	ensure_installed = {
+		"html",
+		"css",
+		"vim",
+		"lua",
+		"javascript",
+		"typescript",
+		"go",
+		"rust",
+		"yaml",
+		"json",
+		"vimdoc",
+	},
 	highlight = {
 		enable = true,
 		additional_vim_regex_highlighting = false,
